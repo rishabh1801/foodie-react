db83885381cce204df697d311a5b7f2077dfb2d5
Rishabh singh <rishavsc18@gmail.com>
2024-01-15 00:25:35 +0530

    created a project

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ProfilePic from "../Assets/rishabh.png";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonials = () => {
+  return (
+    <div className='work-section-wrapper'>
+        <div className="work-section-top">
+           <p className='primary-subheading'>Testimonials</p>
+           
+           <h1 className='primary-heading'>What are they saying</h1>
+           <p className='primary-text'>
+           Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+           </p>
+        </div>
+        <div className='testimonial-section-bottom'>
+            <img src={ProfilePic} alt =""/>
+            <p>
+            Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.   
+            </p>
+            <div className='testimonials-stars-container'>
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+             </div>
+             <h2>Rishabh S</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
